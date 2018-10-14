f886cc79279dc612c105496b1097a68460006ee6
bubbajoe <joewilliamsis@live.com>
2018-10-13 21:45:20 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const http = require('http');
+const nconf = require('nconf');
+const express = require('express');
+
+
+var app = express()
+// Read in keys and secrets. Using nconf use can set secrets via
+// environment variables, command-line arguments, or a keys.json file.
+nconf.argv().env().file('keys.json');
+
+// Connect to a MongoDB server provisioned over at
+// MongoLab.  See the README for more info.
+
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
+
+let uri = `mongodb://${host}:${port}`;
+if (nconf.get('mongoDatabase')) {
+  uri = `${uri}/${nconf.get('mongoDatabase')}`;
+}
+var db, ips, chrt;
+mongoose.connect(uri, function(err) {
+    if(err) throw err;
+    else console.log("connected to",uri)
+    // Set db instance
+    db = mongoose.connection;
+
+    // Set the global promise (probably not needed but hey..)
+    mongoose.Promise = global.Promise;
+    //Bind connection to error event (to get notification of connection errors)
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    // init collections
+    ips = db.collection('IPs');
+    // 
+    chrt = db.collection('charities');
+});
+// Get Mongoose to use the global promise library
+
+app.get("/get/state_id/:state",function(req, res) {
+    chrt.find({state:req.params.state.toUpperCase()}, function(err, data) {
+        if (err) {
+            res.status(500);
+            res.send('Im sorry bruh! we fkd up');
+        } else {
+            var r = data.toArray().then((d) => {
+                res.status(200)
+                res.set('Content-Type', 'application/json');
+                console.log(d)
+                res.end(JSON.stringify(d))
+            });
+        }
+    })
+})
+
+app.use("/",function(req,res,next) {
+    // Track every IP that has visited this site (TEST)
+    const ip = {
+    address: req.connection.remoteAddress
+    };
+
+    ips.insert(ip, (err) => {
+    if (err) {
+        console.log("DB ERROR", err);
+        return
+    } else {
+        console.log("IP Data logged successfully")
+    }
+
+    // push out a range
+    let iplist = '';
+    ips.find().toArray((err, data) => {
+        if (err) {
+        throw err;
+        }
+        data.forEach((ip) => {
+        iplist += `${ip.address}; `;
+        });
+
+        // Do nothing for now
+    })
+    })
+    next()
+})
+
+app.use(express.static('public'));
+
+app.listen(8080,function(err) {
+    if (err) throw err;
+})
